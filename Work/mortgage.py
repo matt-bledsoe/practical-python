04f85904dc1ac357af91fe1a368632af8b09e213
Matt Bledsoe <matt@proton.me>
2023-07-28 13:54:29 -0500

    Add Exercises 1.7 -- 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,26 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+n_months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    if n_months >= extra_payment_start_month and n_months <= extra_payment_end_month :
+        # Make an extra payment for the first 12 months
+        principal = principal * (1 + rate/12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    n_months += 1
+    print(n_months, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', total_paid)
+print('Number of months', n_months)
