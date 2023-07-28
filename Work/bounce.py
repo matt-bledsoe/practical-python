b6260743e6f032cfddb049bd5220c42a3da4c357
Matt Bledsoe <matt@proton.me>
2023-07-28 12:41:45 -0500

    Add Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+rebound_pct = 3/5
+n_bounces = 10
+
+while n_bounces > 0:
+    height *= rebound_pct
+    print(round(height, 4))
+    n_bounces -= 1
+
