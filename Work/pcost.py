12d5f4772c6138de2ce8b1484c9b69f30767bae0
Matt Bledsoe <matthew.bledsoe@firsthorizon.com>
2023-08-01 09:27:48 -0500

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,19 @@
 #
 # Exercise 1.27
 
-total_cost = 0.0
+def portfolio_cost(filename):
+    total_cost = 0.0
 
-f = open("Data/portfolio.csv", "rt")
-headers = next(f)
+    f = open(filename, "rt")
+    headers = next(f)
 
-for line in f:
-    row = line.split(",")
-    row_cost = int(row[1]) * float(row[2])
-    total_cost += row_cost
+    for line in f:
+        row = line.split(",")
+        row_cost = int(row[1]) * float(row[2])
+        total_cost += row_cost
 
-print(f"Total cost: ${total_cost:,.2f}")
-f.close()
+    f.close()
+    return(total_cost)
+
+cost = portfolio_cost("Data/portfolio.csv")
+print(f"Total cost: ${cost:,.2f}")
