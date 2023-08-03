7f12bd52e9a296c1d9acce11e0501eae2aec6199
Matt Bledsoe <matt@proton.me>
2023-08-03 08:25:18 -0500

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,19 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        portfolio = []
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    
+    return portfolio
+
+
+
