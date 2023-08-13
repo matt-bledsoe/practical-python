85d171e3bfa3960f614fa07e27c328ca99b9af84
Matt Bledsoe <matthew.bledsoe@firsthorizon.com>
2023-08-13 13:50:42 -0500

    Exercise 3.8

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -7,6 +7,10 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","
     '''
     Parse a csv file into a list of records 
     '''
+    # Can't select without headers
+    if select and ~has_headers:
+        raise RuntimeError("Cannot select columns without headers")
+    
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
          
