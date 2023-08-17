bc7a4844e2ed9df1d6ed3e28f97de98a5b99adea
Matt Bledsoe <matthew.bledsoe@firsthorizon.com>
2023-08-17 13:22:57 -0500

    Exercise 3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,21 +3,13 @@
 # Exercise 1.27
 import csv
 import sys
+import report
 
 def portfolio_cost(filename):
     total_cost = 0.0
-
-    with open(filename, "rt") as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                shares, price = int(record["shares"]), float(record["price"])
-                total_cost += shares * price
-            except ValueError:
-                print(f"Row {rowno}. Couldn't parse", row)
+    portfolio = report.read_portfolio(filename)
+    for record in portfolio:
+        total_cost += record["shares"] * record["price"]
     return(total_cost)
 
 if len(sys.argv) == 2:
