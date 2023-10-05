9c20ee638ca0d52c6c5e7f58eb9ad5a954148cf0
Matt Bledsoe <matthew.bledsoe@firsthorizon.com>
2023-10-05 14:39:29 -0500

    Exercise 6.11

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,4 +1,5 @@
 from follow import follow
+import report
 import csv
 
 def convert_types(rows, types):
@@ -13,15 +14,22 @@ def select_columns(rows, indices):
     for row in rows:
         yield [row[index] for index in indices]
 
+def filter_symbols(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
+
 def parse_stock_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows, [0, 1, 4])
     rows = convert_types(rows, [str, float, float])
     rows = make_dicts(rows, ["name", "price", "change"])
     return rows
+ 
 
 if __name__ == "__main__":
-    lines = follow("Data/stocklog.csv")
-    rows = parse_stock_data(lines)
+    portfolio = report.read_portfolio("Data/portfolio.csv") 
+    rows = parse_stock_data(follow("Data/stocklog.csv"))
+    rows = filter_symbols(rows, portfolio)
     for row in rows:
         print(row)
